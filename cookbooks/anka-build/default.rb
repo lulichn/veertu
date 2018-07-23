f52e72196ad247ec665b4c94c89c4bd45d6072ca
lulichn <me@lulichn.net>
2018-07-23 18:09:12 +0900

    activate

--- cookbooks/anka-build/default.rb
+++ cookbooks/anka-build/default.rb
@@ -2,3 +2,12 @@ darwin_installer node['AnkaBuildUrl'] do
   remote_file true
   curl_option "-e #{node['AnkaReferer']}"
 end
+
+# execute "Accept EULA" do
+#   command "sudo anka license accept-eula"
+# end
+
+execute "Activate License" do
+  command "sudo anka license activate #{node['AnkaBuildLicenseKey']}"
+  not_if "anka license validate"
+end
